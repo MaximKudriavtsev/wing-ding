b4c0562dd827bc0baece62243ffa26e76782afd6
Dmitry Morozov <mboddah@yandex.ru>
2021-10-12 21:09:29 +0300

    feature(client): init application
    Initialisation of a client application

---
+++ client/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { AuthenticationForm } from './src/AuthenticationForm';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <AuthenticationForm/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1a1523',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
