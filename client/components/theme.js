7fecab97fd61c5caf8381c20a3341afcc6ee8930
Dmitry Morozov <mboddah@yandex.ru>
2021-11-19 02:03:08 +0300

    fix(client): update theme and packajes

--- client/components/theme.js
+++ client/components/theme.js
@@ -5,12 +5,14 @@ export const THEME = {
   REGULAR_FONT: 'comfortaa-regular',
   BOLD_FONT: 'comfortaa-bold',
   FONT_COLOR: '#ddd',
-  BACKGROUND_COLOR: '#1b162a',
-  DARKER_COLOR: '#181327',
+  BACKGROUND_COLOR: '#001628',
+  DARKER_COLOR: '#00111e',
+  BRIGHTER_COLOR: '#68a2b9',
+  DANGER_COLOR: '#e9072b',
   TITLE_FONT_SIZE: 28,
   LABEL_FONT_SIZE: 16,
   //Button
-  BUTTON_COLOR: '#0df5e3',
+  BUTTON_COLOR: '#99d9d9',
   BUTTON_FONT_COLOR: '#1a1523',
   BUTTON_FONT_SIZE: 18,
   ACTIVE_OPACITY: 0.7,
@@ -21,15 +23,29 @@ export const THEME = {
   ICON_ENVELOPE: 'envelope-o',
   ICON_LOCK: 'lock',
   ICON_UNLOCK: 'unlock-alt',
+  ICON_SEARCH: 'search',
+  ICON_FRIENDS: 'users',
 };
 
 export const SCREEN_STYLE = StyleSheet.create({
   wrapper: {
+    position: 'relative',
+    backgroundColor: THEME.BACKGROUND_COLOR,
     width: '100%',
     height: '100%',
     alignContent: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 30,
+    paddingHorizontal: 15,
     paddingVertical: 20,
+  },
+
+  listWrapper: {
+    position: 'relative',
+    backgroundColor: THEME.BACKGROUND_COLOR,
+    width: '100%',
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 15,
   },
 });
