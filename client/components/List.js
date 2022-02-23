40e62038c1431f87525d3c28b5862852e9307755
Dmitry Morozov <mboddah@yandex.ru>
2022-02-23 18:32:58 +0300

    fix(client): fix list component

--- client/components/List.js
+++ client/components/List.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, FlatList } from 'react-native';
+import { Text } from './ui/Text';
 import { SCREEN_STYLE } from './theme';
 
 export const List = ({ data, Component, onOpen, onShowMembers }) => {
+  if (!data.length) {
+    return (
+      <View style={SCREEN_STYLE.listWrapper}>
+        <Text style={{ textAlign: 'center' }}>Здесь пока пусто...</Text>
+      </View>
+    );
+  }
   return (
     <View style={SCREEN_STYLE.listWrapper}>
       <FlatList
-        style={{ flex: 1 }}
+        style={{ flex: 1, paddingHorizontal: 15 }}
         data={data}
         kayExtractor={data => data.id.toString()}
         renderItem={({ item }) => (
