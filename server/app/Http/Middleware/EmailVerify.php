5353d39d4c445d14821ee92778ec8d940fecfb80
ljonasHm <shamordinanton@gmail.com>
2022-04-09 00:42:34 +0300

    feature(server): filter requests for email verification by middleware

---
+++ server/app/Http/Middleware/EmailVerify.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Http\Request;
+
+class EmailVerify
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->user() ||
+            ($request->user() instanceof MustVerifyEmail &&
+            !$request->user()->hasVerifiedEmail())) {
+
+            return \response()->json([
+                'status' => 'error',
+                'error' => 'user email address is not verified'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
