1b2272f9312aec35339818db1a0de37435c80579
ljonasHm <shamordinanton@gmail.com>
2022-07-16 16:02:18 +0300

    feature(server): create event with img file

--- server/app/Http/Controllers/EventController.php
+++ server/app/Http/Controllers/EventController.php
@@ -8,23 +8,45 @@ use App\Models\Participation;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class EventController extends Controller
 {
-    public function create() {
+    public function create(Request $request) {
         try {
-            $req = \request();
+
+            $validator = Validator::make($request->all(), [
+                'photo' => 'image'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => 'error',
+                    'error' => 'photo file is not an image'
+                ]);
+            }
 
             $user = auth()->user();
 
             $event = new Event();
-            $event->title = $req['title'];
-            $event->place = $req['place'];
+            $event->title = $request->input('title');
+            $event->place = $request->input('place');
             $event->host_id = $user->id;
-            $event->date = $req['date'];
+            $event->date = $request->input('date');
 
-            if (isset($req['img'])) {
-                $event->img = $req['img'];
+            if ($request->file('img')) {
+                $filename = $request->file('img')->getClientOriginalName();
+                $filename_without_extension = pathinfo($filename, PATHINFO_FILENAME);
+                $extension = $request->file('img')->getClientOriginalExtension();
+
+                if (!$extension || $extension == '') {
+                    $extension = 'jpg';
+                }
+
+                $new_filename = sha1($filename_without_extension . time()) . '.' . $extension;
+
+                $request->file('img')->storeAs('public/event', $new_filename);
+                $event->img = config('app.url') . 'storage/event/' . $new_filename;
             }
 
             if (isset($req['text'])) {
@@ -127,4 +149,43 @@ class EventController extends Controller
         $event['is_member'] = $event->users()->whereId($user->id)->exists();
         return \response()->json($event);
     }
+
+//    public function uploadImg(Request $request) {
+//
+//        $request->validate([
+//            'img' => 'required|img',
+//            'event' => 'required'
+//        ]);
+//
+//        $user = auth()->user();
+//        $event = Event::find($request->input('event'));
+//
+//        if (!$event) {
+//            return response()->json([
+//                'status' => 'error',
+//                'error' => 'no such event'
+//            ]);
+//        }
+//
+//        if ($user->id !== $event->host_id) {
+//            return response()->json([
+//                'status' => 'error',
+//                'error' => 'no such event'
+//            ]);
+//        }
+//
+//        $filename = $request->file('img')->getClientOriginalName();
+//        $filename_without_extension = pathinfo($filename, PATHINFO_FILENAME);
+//        $extension = $request->file('img')->getClientOriginalExtension();
+//        $new_filename = sha1($filename_without_extension . time()) . '.' . $extension;
+//
+//        $request->file('img')->storeAs('public/event-img', $new_filename);
+//
+//        $event->img = $new_filename;
+//
+//        return \response()->json([
+//            'status' => 'success',
+//            'img' => $new_filename
+//        ]);
+//    }
 }
